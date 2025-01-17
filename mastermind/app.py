a4cf88278157655710bac125ea31a238d5b9498a
Rafal Padkowski <rafaelp@poczta.onet.pl>
2025-01-17 19:37:23 +0100

    feat: modify toml configuration

--- mastermind/app.py
+++ mastermind/app.py
@@ -2,7 +2,7 @@ import tomllib
 from pathlib import Path
 
 from textual.app import App, ComposeResult
-from textual.containers import Horizontal
+from textual.containers import Horizontal, VerticalScroll
 from textual.widgets import Button, Footer, Header, Select, Static
 
 
@@ -14,15 +14,17 @@ class MastermindApp(App):
     def __init__(self) -> None:
         super().__init__()
 
-        with open(Path("mastermind") / "config.toml", mode="rb") as toml:
+        with open(Path(__file__).parent / "config.toml", mode="rb") as toml:
             self.config = tomllib.load(toml)
 
-        self.code_pegs: list[str] = self.config["pegs"]["code_pegs"]
+        self.board: VerticalScroll = VerticalScroll(id="board")
+
+        self.color_pegs: list[str] = self.config["pegs"]["color_pegs"]
         self.hole: str = self.config["pegs"]["hole"]
 
         self.row: list[Select] = [
             Select(
-                options=zip(self.code_pegs, range(1, 9)),
+                options=zip(self.color_pegs, range(1, 9)),
                 prompt=self.hole,
                 classes="color_peg",
             )
@@ -32,22 +34,46 @@ class MastermindApp(App):
     def compose(self) -> ComposeResult:
         yield Header(icon="❔")
 
-        yield Horizontal(
+        # yield Horizontal(
+        #     Static("01", classes="num"),
+        #     Static("🔵", classes="static_color_peg"),
+        #     Static("🔴", classes="static_color_peg"),
+        #     Static("🟢", classes="static_color_peg"),
+        #     Static("🟣", classes="static_color_peg"),
+        #     Static("🔴 ⚪ ⭕ ⭕", classes="feedback"),
+        #     classes="row",
+        # )
+
+        yield self.board
+        yield Footer()
+
+    def new_game(self) -> None:
+        # with Horizontal(classes="row"):
+        #     yield Static("02", classes="num")
+
+        #     for widget in self.row:
+        #         yield widget
+
+        #     yield Button("❔", classes="check")
+
+        horizontal = Horizontal(
             Static("01", classes="num"),
-            Static("🔵", classes="static_color_peg"),
-            Static("🔴", classes="static_color_peg"),
-            Static("🟢", classes="static_color_peg"),
-            Static("🟣", classes="static_color_peg"),
-            Static("🔴 ⚪ ⭕ ⭕", classes="feedback"),
+            *self.row,
+            Button("❔", classes="check"),
             classes="row",
         )
 
-        with Horizontal(classes="row"):
-            yield Static("02", classes="num")
+        self.board.mount(horizontal)
 
-            for widget in self.row:
-                yield widget
+        # horizontal = Horizontal(classes="row")
+        # self.board.mount(horizontal)
 
-            yield Button("❔", classes="check")
+        # horizontal.mount()
 
-        yield Footer()
+        # for widget in self.row:
+        #     horizontal.mount(widget)
+
+        # horizontal.mount()
+
+    def on_mount(self) -> None:
+        self.new_game()
