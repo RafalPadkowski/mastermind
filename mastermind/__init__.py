06b4837fc0456668972d8bfe0dfece5feb9bb250
Rafal Padkowski <rafaelp@poczta.onet.pl>
2025-01-28 21:24:12 +0100

    feat: refactor app start

--- mastermind/__init__.py
+++
@@ -1,6 +0,0 @@
-from mastermind.app import MastermindApp
-
-
-def main():
-    app = MastermindApp()
-    app.run()
---
+++ mastermind/main.py
@@ -0,0 +1,10 @@
+from mastermind.app import MastermindApp
+
+
+def main():
+    app = MastermindApp()
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
