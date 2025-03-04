62e4cba28ffb67bf2765a729284f90f95d7dee61
Rafal Padkowski <rafaelp@poczta.onet.pl>
2025-03-05 00:33:48 +0100

    build: update textual

--- mastermind/constants.py
+++ mastermind/constants.py
@@ -6,7 +6,7 @@ from textual_utils import AppMetadata
 
 APP_METADATA = AppMetadata(
     name="Master Mind",
-    version="1.0.8",
+    version="1.0.9",
     codename="🔴 ⚪",
     author="Rafal Padkowski",
     email="rafaelp@poczta.onet.pl",
